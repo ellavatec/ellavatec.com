3c1a268698b3936dd0221d36bc83941d45b72f11
Erin Bergman <ebergman@pillartechnology.com>
2017-06-20 20:45:55 -0400

    home component

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from 'app/home/home.component';
+import { ConsultancyComponent } from 'app/home/consultancy/consultancy.component';
+import { AboutUsComponent } from 'app/home/about-us/about-us.component';
+import { ContactComponent } from 'app/home/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, children: [
+    { path: '', component: ConsultancyComponent },
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'contact', component: ContactComponent }
+  ] }
 ];
 
 @NgModule({
